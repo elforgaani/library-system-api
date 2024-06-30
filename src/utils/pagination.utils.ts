4f04db2c62b3fa0c811c8c2a9a2e0f3a3ef808fd
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-06-30 09:59:50 +0200

    Added Pagination interface!

--- src/utils/pagination.utils.ts
+++ src/utils/pagination.utils.ts
@@ -1,3 +1,8 @@
-export const pagination = (page: number) => {
+interface Pagination {
+  limit: number,
+  skip: number
+}
+
+export const pagination = (page: number, limit: number): Pagination => {
   return { limit: 6, skip: (page - 1) * 6 };
 };
