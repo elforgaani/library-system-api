f688d19b676e6de9484b8d34ffc4801150f79953
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-07-01 09:42:38 +0200

    Added GlobalErrorMiddleware.ts

---
+++ src/utils/global-error.middleware.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+
+export const globalErrorMiddlewareHnadler = async (error: any, req: Request, res: Response, next: NextFunction) => {
+    if (error) {
+        return res.status(500).json({ success: false, message: "Internal Server Error From Global Error Handelr Middleware" });
+    }
+}
