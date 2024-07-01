1f875f68609ff561911dcdb6917398a5f930e4e0
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-07-01 09:43:57 +0200

    Added New GlobalError Middleware to Server.ts!

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,18 @@ import "dotenv/config";
 import dbConnection from "./database/connection";
 import BooksRouter from "./modules/book/book.routes";
 import AuthorsRouter from "./modules/author/author.routes";
+import { globalErrorMiddlewareHnadler } from "./utils/global-error.middleware";
 export default function main() {
   const port = Number(process.env.PORT);
   const app = express();
   dbConnection();
 
   app.use(express.json());
+  
   app.use("/api/v1/books", BooksRouter);
   app.use("/api/v1/authors", AuthorsRouter);
+
+  app.use(globalErrorMiddlewareHnadler);
 
   app.listen(port, () => console.log("Server Connected Successfully"));
 }
